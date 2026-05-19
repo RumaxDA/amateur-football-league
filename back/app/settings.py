12a32aa1354465869f6df79fa94fc340fdaa4d69
xRumax <167364@student.uwm.edu.pl>
2026-05-19 22:27:58 +0200

    fix(db): switch to psycopg2 and fix SQLAlchemy sync configuration

--- back/app/settings.py
+++ back/app/settings.py
@@ -1,6 +1,5 @@
 from datetime import timedelta
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 SECRET_KEY = "@!#$!#$^@%$&1KOI#$%^JAS!#@$!@#RDAFSDFADFA"
 TOKEN_EXPIRATION = timedelta(hours=1)
 REFRESH_TOKEN_EXPIRE_DAYS = 30
